0cf146c368866a2b1e86ade70b0c039bd0fe6cea
Jimmy Zhuo <zhuomingliang01@qq.com>
2022-01-06 10:35:10 +0800

    添加动态路由

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\User;
 
 class HomeController extends Controller {
     /**
@@ -21,5 +22,9 @@ class HomeController extends Controller {
      */
     public function index() {
         return view('home');
+    }
+
+    public function getUser(User $user) {
+        dd($user);
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,27 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+
+//万能路由
+// Route::group(['middleware'=>['web']], function () {
+    Route::any('/{controller}/{action}', function ($controller, $action) {
+        $controller = 'App\\Http\\Controllers\\' . ucfirst(strtolower($controller)) . 'Controller';
+
+        if (class_exists($controller)) {
+            $controller = \App::make($controller);
+
+            $action = strtolower(request()->getMethod()) . ucfirst(strtolower($action));
+
+            try {
+                return \App::call([$controller, $action]);
+            } catch (\ReflectionException $e) {
+                if (env('APP_DEBUG') === true) {
+                    throw $e;
+                }
+            }
+        }
+
+        return abort(404);
+    }); //->where([ 'controller' => '[0-9a-zA-Z]+', 'action' => '[0-9a-zA-Z]+']);
+// });
