12a93581870a09ab65213fd72424e3f2df04dafb
Jimmy Zhuo <zhuomingliang01@qq.com>
2022-02-28 10:29:38 +0800

    完善权限管理模块

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -15,9 +15,19 @@ class UserController extends Controller {
      * @return void
      */
     public function getIndex(Request $request) {
-        return User::latest()->paginate(
+        return User::from(\DB::raw('
+        users,
+        LATERAL (SELECT
+            string_agg(r.id::text, \',\') as role_id,
+            string_agg(r.name, \'、\') as role
+        FROM
+            roles r
+        LEFT JOIN user_has_roles ur ON r.id = ur.role_id
+        WHERE
+            ur.user_id = users.id) as r
+        '))->latest()->paginate(
             (int) $request->get('per_page'),
-            ['*'],
+            ['users.username', 'users.email', 'users.created_at', 'r.*'],
             'current_page'
         );
     }
