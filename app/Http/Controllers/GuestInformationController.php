da94b9149d74f5ae69091528bb41cb855d78cd30
liugb <915002852@qq.com>
2022-04-22 11:01:50 +0800

    Signed-off-by: liugb <915002852@qq.com>

--- app/Http/Controllers/GuestInformationController.php
+++ app/Http/Controllers/GuestInformationController.php
@@ -6,5 +6,32 @@ use Illuminate\Http\Request;
 
 class GuestInformationController extends Controller
 {
-    //
+    //获取信息
+    public function getIndex()
+    {
+
+    }
+
+    //新增信息
+    public function PostIndex()
+    {
+
+    }
+
+    //导入信息
+    public function PostImport()
+    {
+
+    }
+
+    //修改
+    public function PutIndex()
+    {
+
+    }
+
+    public function DeleteIndex()
+    {
+        
+    }
 }
