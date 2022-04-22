9a36040d4d4cedadc244735b284025e4a32f1e0e
liugb <915002852@qq.com>
2022-04-22 11:20:59 +0800

    Signed-off-by: liugb <915002852@qq.com>新增控制器方法

--- app/Http/Controllers/EpidemicPreventionInstructionsController.php
+++ app/Http/Controllers/EpidemicPreventionInstructionsController.php
@@ -6,5 +6,21 @@ use Illuminate\Http\Request;
 
 class EpidemicPreventionInstructionsController extends Controller
 {
-    //
+    //获取
+    public function getIndex()
+    {
+
+    }
+
+    //新增
+    public function PostIndex()
+    {
+
+    }
+
+    //修改
+    public function PutIndex()
+    {
+
+    }
 }
