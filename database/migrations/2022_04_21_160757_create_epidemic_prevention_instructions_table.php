553d404c8388cf26cfacb10a6b0c33678d8ebb08
zhuoml <zhuoml@qq.com>
2022-04-29 15:52:19 +0800

    完善代码

--- database/migrations/2022_04_21_160757_create_epidemic_prevention_instructions_table.php
+++ database/migrations/2022_04_21_160757_create_epidemic_prevention_instructions_table.php
@@ -3,6 +3,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use App\Models\EpidemicPreventionInstructions;
 
 return new class extends Migration {
     /**
@@ -17,6 +18,8 @@ return new class extends Migration {
         });
 
         DB::statement("COMMENT ON TABLE epidemic_prevention_instructions IS '防疫信息'");
+
+        EpidemicPreventionInstructions::create(['id' => 1, 'content' => '内容']);
     }
 
     /**
