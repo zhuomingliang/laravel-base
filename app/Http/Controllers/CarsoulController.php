e6b8d2988dd8a020ed380301a2167e7c7b03ec94
zhuoml <zhuoml@qq.com>
2022-07-04 16:28:28 +0800

    控制器、模型初始化

---
+++ app/Http/Controllers/CarsoulController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Carsoul;
+use Illuminate\Http\Request;
+
+class CarsoulController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Carsoul  $carsoul
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Carsoul $carsoul)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Carsoul  $carsoul
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Carsoul $carsoul)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Carsoul  $carsoul
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Carsoul $carsoul)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Carsoul  $carsoul
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Carsoul $carsoul)
+    {
+        //
+    }
+}
---
+++ app/Http/Controllers/NavigationController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\MainMenu;
+use Illuminate\Http\Request;
+
+class NavigationController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\MainMenu  $mainMenu
+     * @return \Illuminate\Http\Response
+     */
+    public function show(MainMenu $mainMenu)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\MainMenu  $mainMenu
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(MainMenu $mainMenu)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\MainMenu  $mainMenu
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, MainMenu $mainMenu)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\MainMenu  $mainMenu
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(MainMenu $mainMenu)
+    {
+        //
+    }
+}
