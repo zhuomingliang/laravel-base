708289be42540cd3ee3d6480ef543727e6eac88e
zhuoml <zhuoml@qq.com>
2022-07-07 09:36:59 +0800

    功能完善

--- database/migrations/2022_07_07_091246_create_home_page_table.php
+++ database/migrations/2022_07_07_091246_create_home_page_table.php
@@ -15,6 +15,7 @@ return new class extends Migration {
             $table->id();
             $table->integer('module_id')->default(0)->comment('模块 ID');
             $table->bigInteger('sub_menu_id')->comment('二级导航栏 ID');
+            $table->smallInteger('order')->default(1)->comment('顺序');
             $table->boolean('status')->default(true)->comment('状态');
             $table->softDeletes('deleted_at', 0)->comment('软删除');
             $table->timestamp('created_at')->default(DB::raw('LOCALTIMESTAMP(0)'))->comment('创建时间');
